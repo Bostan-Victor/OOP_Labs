34b89cf563713224661aada882870059ecc277a6
Bostan Victor <victorbostan01@gmail.com>
2023-10-23 12:19:05 +0300

    Create Main class

---
+++ Lab2/main.py
@@ -0,0 +1,23 @@
+import DirectoryMonitor
+
+class Main:
+    def __init__(self):
+        self.directory = 'D:\#Year2\OOP\OOP_Labs\Lab2\Lab2_test_folder'
+        self.monitor = DirectoryMonitor(self.directory)
+
+
+    def run(self):
+        inp = ''
+        while inp != 'exit':
+            inp = input('Choose an option(commit, info <filename>, status, exit): ').strip().lower()
+            if inp == 'commit':
+                pass
+            elif inp.startswith('info '):
+                pass
+            elif inp == 'status':
+                pass
+
+
+if __name__ == '__main__':
+    main = Main()
+    main.run()
