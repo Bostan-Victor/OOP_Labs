af0b7914bf918b10ddde498d3d34c4563cf9889b
Bostan Victor <victorbostan01@gmail.com>
2023-10-26 12:27:20 +0300

    Create ImageFile class logic

--- Lab2/ImageFile.py
+++ Lab2/ImageFile.py
@@ -1,3 +1,18 @@
+from BaseFile import BaseFile
+from PIL import Image
 
-class ImageFile:
-    pass
+
+class ImageFile(BaseFile):
+    def info(self):
+        base_info = super().info()
+        width, height = self.get_dimensions()
+        base_info.update({
+            'dimensions': f'{width}x{height}'
+        })
+
+        return base_info
+
+
+    def get_dimensions(self):
+        with Image.open(self.path) as img:
+            return img.width, img.height
